1291b3a94a680f69ba0add066ca1357523677967
Marijn Ophorst <marijn@monomelodies.nl>
2022-11-01 16:23:02 +0100

    implement constructor

--- src/FlagNotDefinedException.php
+++ src/FlagNotDefinedException.php
@@ -6,5 +6,9 @@ use DomainException;
 
 class FlagNotDefinedException extends DomainException
 {
+    public function __construct(string $class, string $property, string $flag)
+    {
+        parent::__construct("Bitflag $class::$property does not specify the option $flag");
+    }
 }
 
